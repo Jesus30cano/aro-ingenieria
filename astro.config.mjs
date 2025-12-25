069d71df5e09d7faed5a81426dd840ef19223ae8
jesus cano <jcano2620k@gmail.com>
2025-12-25 13:21:43 -0500

    ya

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
-
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
